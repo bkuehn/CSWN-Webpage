7c0a6f80322357802c7756cb1063d3086ff5538d
Bridgette <bkuehn@purdue.edu>
2016-04-27 22:41:38 -0400

    Updated commit for local files into git

---
+++ components/header.js
@@ -0,0 +1,24 @@
+document.write('\
+	<div id="header-wrapper">\
+    <div class="container">\
+        <div class="row">\
+            <div class="12u">\
+                <div class="12u">\
+                    <header id="header">\
+                        <h1><a href="index.html" id="logo">CSWN</a></h1>\
+                        <nav id="nav">\
+                            <a href="constitution.html">Constitution</a>\
+                            <a href="people.html">Officers</a>\
+                            <a href="events.html">Events</a>\
+                            <a href="sponsors.html">Sponsors</a>\
+                            <a href="resources.html">Resources</a>\
+                            <a href="alumni.html">Alumni</a>\
+                            <a href="contactus.html">Contact Us</a>\
+                        </nav>\
+                    </header>\
+                </div>\
+            </div>\
+        </div>\
+    </div>\
+    </div>\
+');
